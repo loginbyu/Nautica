89ded544dc170b8265e9f41eb1e9792e83698740
loginbyu <loginbyu03@gmail.com>
2025-05-18 13:06:04 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // Variables
 const rootDomain = "pages.dev"; // Ganti dengan domain utama kalian
-const serviceName = "naruto-uzumaki"; // Ganti dengan nama workers kalian
+const serviceName = "naruto-uzumaki-7x6"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
